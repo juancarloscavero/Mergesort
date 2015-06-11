14a67620af3d57cdfb0467d4b7d34e5e5f693368
Juan Carlos <juancarlos.cavero@medianet.es>
2015-06-11 18:13:03 +0200

    Optimización

--- test_mergesort.rb
+++ test_mergesort.rb
@@ -1,57 +1,26 @@
 require 'test/unit'
-require_relative 'Mergesort_2.0'
+require_relative 'Mergesort'
 
 class Test_mergesort < Test::Unit::TestCase
 
 	def test_hayquedividir?
-		array=[1,2,3,4]
-		assert_equal(true, hayquedividir?(array) )
+		assert_equal(true, hayquedividir?([1,2,3,4]) )
 
 	end
 
-	# def test_dividir_array_par
-	# 	array=[1,2,3,4]
-	# 	lista = Mergesort.new()
-	# 	assert_equal( [1,2,3,4], lista.dividir(array) )
-	# end
-
-	# def test_dividir_array_impar
-	# 	array=[1,2,3,4,5]
-	# 	lista = Mergesort.new()
-	# 	assert_equal( [1,2,3,4,5], lista.dividir(array))
-	# end
-
-
-	def test_mezclar_nums_ordenados
-		array1 = [2]
-		array2 = [1]
-		assert_equal( [1,2],mezclar(array1,array2) )
-	end
-
-	# def test_mezclar_nums_desordenados
-	# 	array1=[1,2]
-	# 	array2=[3,4,5]
-	# 	lista = Mergesort.new()
-	# 	assert_equal( [1,2,3,4,5], lista.mezclar(array1,array2) )
-	# end
-
-	def test_ordenar_num_facil
-		array = [5,4,3,1,2]	
-		assert_equal([1,2,3,4,5], mergesort(array) )		
+	def test_ordenar_num_facil	
+		assert_equal([1,2,3,4,5], mergesort([5,4,3,1,2]) )		
 	end
 
 	def test_ordenar_num_dificil
-		array = [9,4,3,7,1]
-		assert_equal([1,3,4,7,9], mergesort(array))
+		assert_equal([1,3,4,7,9], mergesort([9,4,3,7,1]))
 	end
 
 	def test_ordenar_cadenas
-		array = ["Avion","casa","barco"]
-		assert_equal(["Avion","barco", "casa"], mergesort(array))
+		assert_equal(["Avion","barco", "casa"], mergesort(["Avion","casa","barco"]))
 	end
 	def test_ordenar_floats
-		array = [3.0, 7.2, 2.0]
-		assert_equal([2.0, 3.0, 7.2], mergesort(array))
+		assert_equal([2.0, 3.0, 7.2], mergesort([3.0, 7.2, 2.0]))
 	end
 
 end
