7f446088ea03b8619793cf1dc2e9f9c378914589
Juan Carlos <juancarlos.cavero@medianet.es>
2015-06-12 10:31:23 +0200

    Algoritmo con clases

--- mergesort.rb
+++ mergesort.rb
@@ -1,29 +1,32 @@
-def hayquedividir?(array)
-		if array.length > 1
-			return true
-		end
-end
-
-
-def mergesort(array)
-	if hayquedividir?(array)
-		medio= ((array.length) /2)
-		arrayizq = mergesort(array.take(medio))
-		arraydrch = mergesort(array.drop(medio))
-		mezclar(arrayizq,arraydrch)
-	else
-		array
-	end
-end
-
-def mezclar(arrayizq,arraydrch)
- if arrayizq.empty?
-     arraydrch
-   elsif arraydrch.empty?
-     arrayizq
-  elsif arrayizq.first < arraydrch.first
-    [arrayizq.first] + mezclar(arrayizq[1..arrayizq.length], arraydrch)
-  else
-    [arraydrch.first] + mezclar(arrayizq, arraydrch[1..arraydrch.length])
+class Mergesort #Mezclar de todo
+  def initialize
+ 
   end
-end	
+
+  def self.hayquedividir?(array)
+    return true if array.length > 1
+  end
+
+  def self.ordenar(array)
+    if hayquedividir?(array)
+      mitad_array = ((array.length) / 2)
+      arrayizq = ordenar(array.take(mitad_array))
+      arraydrch = ordenar(array.drop(mitad_array))
+      mezclar(arrayizq, arraydrch)
+    else
+      array
+    end
+  end
+
+  def self.mezclar(arrayizq, arraydrch)
+    if arrayizq.empty?
+      arraydrch
+    elsif arraydrch.empty?
+      arrayizq
+    elsif arrayizq.first < arraydrch.first
+      [arrayizq.first] + mezclar(arrayizq[1..arrayizq.length], arraydrch)
+    else
+      [arraydrch.first] + mezclar(arrayizq, arraydrch[1..arraydrch.length])
+    end
+  end
+end
--- test_mergesort.rb
+++ test_mergesort.rb
@@ -4,34 +4,32 @@ SimpleCov.start
 require 'test/unit'
 require_relative 'mergesort'
 
+class Test_mergesort < Test::Unit::TestCase #Testeamos nuestro mergesort
+  def test_hayquedividir?
+    assert_equal(true, Mergesort.hayquedividir?([1, 2, 3, 4]))
+  end
 
-class Test_mergesort < Test::Unit::TestCase
+  def test_ordenar_num_facil
+    assert_equal([1, 2, 3, 4], Mergesort.ordenar([4, 3, 1, 2]))
+  end
 
-	def test_hayquedividir?
-		assert_equal(true, hayquedividir?([1,2,3,4]) )
+  def test_ordenar_num_dificil
+    assert_equal([1, 3, 4, 7, 9], Mergesort.ordenar([9, 4, 3, 7, 1]))
+  end
 
-	end
+  def test_ordenar_cadenas
+    assert_equal(%w(Avion barco casa), Mergesort.ordenar(%w(Avion casa barco)))
+  end
 
-	def test_ordenar_num_facil	
-		assert_equal([1,2,3,4], mergesort([4,3,1,2]) )		
-	end
+  def test_ordenar_floats
+    assert_equal([2.0, 3.0, 7.2], Mergesort.ordenar([3.0, 7.2, 2.0]))
+  end
 
-	def test_ordenar_num_dificil
-		assert_equal([1,3,4,7,9], mergesort([9,4,3,7,1]))
-	end
+  def test_caracteres_especiales
+    assert_equal(['#', '*', '/'], Mergesort.ordenar(['/', '*', '#']))
+  end
 
-	def test_ordenar_cadenas
-		assert_equal(["Avion","barco", "casa"], mergesort(["Avion","casa","barco"]))
-	end
-	def test_ordenar_floats
-		assert_equal([2.0, 3.0, 7.2], mergesort([3.0, 7.2, 2.0]))
-	end
-	def test_caracteres_especiales
-		assert_equal(['#','*','/'], mergesort(['/','*','#']))
-	end
-	def test_mayusculas
-		assert_equal(["A","B","C","a", "b", "c"], mergesort(["a","c","b","C","A","B"]) )
-		
-	end
-
+  def test_mayusculas
+    assert_equal(%w(A B C a b c), Mergesort.ordenar(%w(a c b C A B)))
+  end
 end
